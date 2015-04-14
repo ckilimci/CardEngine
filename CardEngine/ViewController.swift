bc1c807c890008c4901e07a17bbcc9f0a438f8f3
Caglar Kilimci <ckilimci@gmail.com>
2015-04-14 23:00:23 +0300

    as a project

---
+++ CardEngine/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CardEngine
+//
+//  Created by Caglar Kilimci on 10.04.2015.
+//  Copyright (c) 2015 Caglar Kilimci. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
